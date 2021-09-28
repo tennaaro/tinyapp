d22024b3e782603fd9c801a22498ac8c51453367
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-28 19:43:04 -0400

    added delete button

--- express_server.js
+++ express_server.js
@@ -55,6 +55,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls')
+});
+
 function generateRandomString() {
   return Math.random().toString(36).substr(2, 6);
 }
