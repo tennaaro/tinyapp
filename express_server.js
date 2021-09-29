b3abc138cf409db4828500ad25a7a8b6529acd36
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-29 16:58:20 -0400

    Adding new Registration and Login pages

--- express_server.js
+++ express_server.js
@@ -13,6 +13,57 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
+const findUserByEmail = function (email, users) {
+  for (let userId in users) {
+    const user = users[userId];
+    if (email === user.email) {
+      return user;
+    }
+  }
+
+  return false;
+};
+
+const createUser = function (email, password, users) {
+  const userId = generateRandomString();
+
+  users[userId] = {
+    id: userId,
+    email,
+    password,
+  };
+
+  return userId;
+};
+
+const authenticateUser = function (email, password, usersDb) {
+  const userFound = findUserByEmail(email, usersDb);
+
+  if (userFound && userFound.password === password) {
+    return userFound;
+  }
+
+  return false;
+};
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,7 +73,8 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {user: req.cookies["user_id"]}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -34,13 +86,13 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: req.cookies["user_id"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   console.log(req.params);
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] }
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: req.cookies["user_id"]}
   res.render("urls_show", templateVars);
 });
 
@@ -67,16 +119,87 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect('/urls')
 });
 
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username)
+app.post("/logout", (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/login');
+})
+
+app.get('/register', (req, res) => {
+  const templateVars = { user: req.cookies["user_id"]};
+  res.render('urls_register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  console.log('req.body:', req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const userFound = findUserByEmail(email, users);
+
+  if (email === "" || password === "") {
+    res.status(400).send('Empty password or email!');
+    return;
+  }
+
+  if (userFound) {
+    res.status(401).send('Sorry, that user already exists!');
+    return;
+  }
+
+  const userId = createUser(email, password, users);
+
+  res.cookie('user_id', userId);
   res.redirect('/urls');
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls');
+app.get('/login', (req, res) => {
+  const templateVars = {user: req.cookies["user_id"]};
+  res.render('urls_login', templateVars);
 })
 
-function generateRandomString() {
-  return Math.random().toString(36).substr(2, 6);
-}
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = authenticateUser(email, password, users);
+  if (user) {
+    res.cookie('user_id', user.id);
+    res.redirect('/urls');
+    return;
+  }
+
+  const emailCheck = findUserByEmail(email, users);
+  if (emailCheck && !user) {
+    res.status(403).send('Password does not match!')
+    return;
+  }
+
+  res.status(401).send('No user with that email found!');
+
+})
+
+
+
+/*
+<% if (user) { %>
+      <div>
+        <div>
+          <%= `Logged in as: ${user.email} `%>
+        </div>
+        <form class="form-inline" action="/logout" method="POST">
+            <button type="submit" class="btn btn-primary">Logout</button>
+        </form>
+      </div>
+      <% } else { %>
+        <main style="margin: 1em;">
+          <form class="form-inline" action="/login" method="POST">
+            <div class="form-group mb-2">
+              <label for="longURL">username: </label>
+              <input class="form-control" type="text" name="username" placeholder="username"
+                style="width: 300px; margin: 1em">
+              <button type="submit" class="btn btn-primary">sign in</button>
+            </div>
+          </form>
+        </main>
+        <% } %>
+*/
